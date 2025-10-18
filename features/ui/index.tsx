9c1959830d2665d4e98664dadaa2ec64f242f803
Diego Grassino <diegograssino@gmail.com>
2025-10-18 04:13:42 -0300

    Fix case issue with build

--- features/ui/index.tsx
+++ features/ui/index.tsx
@@ -2,6 +2,6 @@ export { default as Anchor } from "./Anchor/Anchor";
 export { default as Container } from "./Container/Container";
 export { default as FavsAnchor } from "./FavsAnchor/FavsAnchor";
 export { default as Footer } from "./Footer/Footer";
-export { default as Header } from "./Header";
+export { default as Header } from "./Header/Header";
 export { default as PageMessage } from "./PageMessage/PageMessage";
 export { default as Typography } from "./Typography/Typography";
